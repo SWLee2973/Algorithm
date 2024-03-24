ec74a7645b6d64f31ec449af38fe990effc59987
상언 <46062634+SWLee2973@users.noreply.github.com>
2024-03-24 09:55:40 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    var answer = '';
+    
+    const len = s.length;
+    
+    answer = len % 2 === 0 ? `${s[len/2 - 1]}${s[len/2]}` : s[parseInt(len/2)]
+    
+    return answer;
+}
