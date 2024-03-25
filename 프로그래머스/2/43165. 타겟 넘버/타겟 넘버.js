922e323ccaadb0a27122b9c73486b2e4d8d6f916
상언 <46062634+SWLee2973@users.noreply.github.com>
2024-03-25 16:43:44 +0900

    [level 2] Title: 타겟 넘버, Time: 1.14 ms, Memory: 35.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,17 @@
+function solution(numbers, target) {
+    var answer = 0
+    
+    const dfs = (sum, depth) => {
+        if(depth === numbers.length) {
+            if(sum === target) answer++;
+            return;
+        }
+        
+        dfs(sum + numbers[depth], depth + 1);
+        dfs(sum - numbers[depth], depth + 1);
+    }
+    
+    dfs(0, 0);
+    
+    return answer;
+}
